b8518ee58785e8a6c7434fde340297a8083d7708
munoz437 <jorgeelias437@gmail.com>
2022-09-03 21:45:18 -0600

    nuevos cambios

---
+++ control_rendimiento/index.php
@@ -0,0 +1,133 @@
+<?php
+$alert = '';
+session_start();
+if (!empty($_SESSION['active'])) {
+  header('location: sistema/');
+} else {
+  if (!empty($_POST)) {
+    if (empty($_POST['usuario']) || empty($_POST['clave'])) {
+      $alert = '<div class="alert alert-danger" role="alert">
+  Ingrese su usuario y su clave
+</div>';
+    } else {
+      require_once "conexion.php";
+      $user = mysqli_real_escape_string($conexion, $_POST['usuario']);
+      $clave = md5(mysqli_real_escape_string($conexion, $_POST['clave']));
+      $query = mysqli_query($conexion, "SELECT u.idusuario, u.nombre, u.correo,u.usuario,r.idrol,r.rol FROM usuario u INNER JOIN rol r ON u.rol = r.idrol WHERE u.usuario = '$user' AND u.clave = '$clave'");
+      mysqli_close($conexion);
+      $resultado = mysqli_num_rows($query);
+      if ($resultado > 0) {
+        $dato = mysqli_fetch_array($query);
+        $_SESSION['active'] = true;
+        $_SESSION['idUser'] = $dato['idusuario'];
+        $_SESSION['nombre'] = $dato['nombre'];
+        $_SESSION['email'] = $dato['correo'];
+        $_SESSION['user'] = $dato['usuario'];
+        $_SESSION['rol'] = $dato['idrol'];
+        $_SESSION['rol_name'] = $dato['rol'];
+        header('location: sistema/');
+      } else {
+        $alert = '<div class="alert alert-danger" role="alert">
+              Usuario o Contraseña Incorrecta
+            </div>';
+        session_destroy();
+      }
+    }
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Match Point</title>
+
+  <!-- Custom fonts for this template
+  <link href="sistema/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">  
+  <link href="sistema/css/sb-admin-2.min.css" rel="stylesheet">-->
+  <link rel="stylesheet" href="https://bootswatch.com/5/lux/bootstrap.min.css">
+  <link rel="icon" href="img/logo.ico">
+  <style>
+    .colorVerde{
+      background: #11998e;  /* fallback for old browsers */
+      background: -webkit-linear-gradient(to right, #38ef7d, #11998e);  /* Chrome 10-25, Safari 5.1-6 */
+      background: linear-gradient(to right, #38ef7d, #11998e); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+    }
+    .colorVerde2{
+      background: #56ab2f;  /* fallback for old browsers */
+      background: -webkit-linear-gradient(to right, #a8e063, #56ab2f);  /* Chrome 10-25, Safari 5.1-6 */
+      background: linear-gradient(to right, #a8e063, #56ab2f); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+    }
+  </style>
+
+</head>
+
+<body class="colorVerde">
+
+  <div class="container">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+      <div class="col-xl-10 col-lg-12 col-md-9">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg-6 d-none d-lg-block bg-login-image">
+                <img src="sistema/img/logo1.jpg" class="img-thumbnail">
+              </div>
+              <div class="col-lg-6">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Iniciar Sesión</h1>
+                  </div>
+                  <form class="user" method="POST">
+                    <?php echo isset($alert) ? $alert : ""; ?>
+                    <div class="form-group">
+                      <label for="">Usuario</label>
+                      <input type="text" class="form-control" placeholder="Escribe tu usuario" name="usuario"></div>
+                    <div class="form-group">
+                      <label for="">Contraseña</label>
+                      <input type="password" class="form-control" placeholder="Escribe tu contraseña" name="clave">
+                    </div>
+                    <br>
+                    <div class="d-grid gap-2">
+                      <input type="submit" value="Iniciar Sesión" class="btn btn-success"> 
+                    </div>                   
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="sistema/vendor/jquery/jquery.min.js"></script>
+  <script src="sistema/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="sistema/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="sistema/js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
