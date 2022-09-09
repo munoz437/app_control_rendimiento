821ad491f302f27a87d6ba4939ae2cdcf6ed97ec
munoz437 <jorgeelias437@gmail.com>
2022-09-08 22:46:57 -0600

    agregando modulo tenista2

--- control_rendimiento/sistema/registro_cliente.php
+++ control_rendimiento/sistema/registro_cliente.php
@@ -6,39 +6,29 @@ if (!empty($_POST)) {
         $alert = '<div class="alert alert-danger" role="alert">
                                     Todo los campos son obligatorio
                                 </div>';
-    } else {
-        $dni = $_POST['dni'];
+    } 
+    else {
+     
         $nombre = $_POST['nombre'];
         $telefono = $_POST['telefono'];
         $direccion = $_POST['direccion'];
         $usuario_id = $_SESSION['idUser'];
-
         $apellidos = $_POST['apellidos'];
         $fecha_nacimiento = $_POST['fecha_nacimiento'];
         $altura = $_POST['altura'];
         $peso = $_POST['peso'];
-
-        $result = 0;
-        if (is_numeric($dni) and $dni != 0) {
-            $query = mysqli_query($conexion, "SELECT * FROM cliente where dni = '$dni'");
-            $result = mysqli_fetch_array($query);
-        }
-        if ($result > 0) {
-            $alert = '<div class="alert alert-danger" role="alert">
-                                    El dni ya existe
-                                </div>';
-        } else {
-            $query_insert = mysqli_query($conexion, "INSERT INTO cliente(dni,nombre,telefono,direccion, usuario_id,apellidos,fecha_nacimiento,peso,altura) values ('$dni', '$nombre', '$telefono', '$direccion', '$usuario_id','$apellidos','$fecha_nacimiento','$altura','$peso')");
+     
+            $query_insert = mysqli_query($conexion, "INSERT INTO cliente(nombre,telefono,direccion, usuario_id,apellidos,fecha_nacimiento,peso,altura) values ('$nombre', '$telefono', '$direccion', '$usuario_id','$apellidos','$fecha_nacimiento','$altura','$peso')");
             if ($query_insert) {
                 $alert = '<div class="alert alert-primary" role="alert">
                                     Tenista Registrado
                                 </div>';
-            } else {
+            }
+             else {
                 $alert = '<div class="alert alert-danger" role="alert">
                                     Error al Guardar
                             </div>';
-            }
-        }
+            }        
     }
     mysqli_close($conexion);
 }
@@ -57,11 +47,7 @@ if (!empty($_POST)) {
     <div class="row">
         <div class="col-lg-6 m-auto">
             <form action="" method="post" autocomplete="off">
-                <?php echo isset($alert) ? $alert : ''; ?>
-                <div class="form-group">
-                    <label for="dni">Dni</label>
-                    <input type="number" placeholder="Ingrese dni" name="dni" id="dni" class="form-control">
-                </div>
+                <?php echo isset($alert) ? $alert : ''; ?>               
                 <div class="form-group">
                     <label for="nombre">Nombres</label>
                     <input type="text" placeholder="Ingrese Nombres" name="nombre" id="nombre" class="form-control">
