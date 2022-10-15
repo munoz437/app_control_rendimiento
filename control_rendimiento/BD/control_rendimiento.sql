1a4708fc46c778c39d7bb76f3f811770323a9cd3
munoz437 <jorgeelias437@gmail.com>
2022-10-14 22:25:36 -0600

    tbl rendimiento

--- control_rendimiento/BD/control_rendimiento.sql
+++ control_rendimiento/BD/control_rendimiento.sql
@@ -5,3 +5,5 @@ ALTER TABLE `cliente` ADD `apellidos` VARCHAR(50) NULL AFTER `usuario_id`, ADD `
 CREATE TABLE `webapsgt_control_rendimiento`.`ejercicio` ( `id` INT(10) NOT NULL AUTO_INCREMENT , `nombre` VARCHAR(50) NOT NULL , `descripcion` TEXT NOT NULL , `video` VARCHAR(100) NULL , `tiempo` VARCHAR(100) NOT NULL , `id_tenista` INT(10) NOT NULL , `id_entrenador` INT(10) NOT NULL , `fecha` VARCHAR(25) NOT NULL , `comentario` VARCHAR(150) NULL , `rendimiento` VARCHAR(10) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
 --
+CREATE TABLE `webapsgt_control_rendimiento`.`rendimiento` ( `id` INT(10) NOT NULL AUTO_INCREMENT , `id_tenista` INT(10) NOT NULL , `ace` INT(10) NULL , `rendimiento` INT(10) NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+--
