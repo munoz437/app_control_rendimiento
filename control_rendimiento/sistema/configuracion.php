b7f17d77bc0275b31c5ab1ee89457d2a14da73b3
munoz437 <jorgeelias437@gmail.com>
2022-10-20 12:09:50 -0600

    configuracion

--- control_rendimiento/sistema/configuracion.php
+++ control_rendimiento/sistema/configuracion.php
@@ -5,96 +5,13 @@
 
 	<!-- Page Heading -->
 	<div class="d-sm-flex align-items-center justify-content-between mb-4">
-		<h1 class="h3 mb-0 text-gray-800">Panel de Administración</h1>
-	</div>
-
-	<!-- Content Row -->
-	<div class="row">
-
-		<!-- Earnings (Monthly) Card Example -->
-		<a class="col-xl-3 col-md-6 mb-4" href="lista_usuarios.php">
-			<div class="card border-left-primary shadow h-100 py-2">
-				<div class="card-body">
-					<div class="row no-gutters align-items-center">
-						<div class="col mr-2">
-							<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Usuarios</div>
-							<div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $data['usuarios']; ?></div>
-						</div>
-						<div class="col-auto">
-							<i class="fas fa-user fa-2x text-gray-300"></i>
-						</div>
-					</div>
-				</div>
-			</div>
-		</a>
-
-		<a class="col-xl-3 col-md-6 mb-4" href="lista_tenista.php">
-			<div class="card border-left-success shadow h-100 py-2">
-				<div class="card-body">
-					<div class="row no-gutters align-items-center">
-						<div class="col mr-2">
-							<div class="text-xs font-weight-bold text-success text-uppercase mb-1">Tenistas</div>
-							<div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $data['clientes']; ?></div>
-						</div>
-						<div class="col-auto">
-							<i class="fas fa-users fa-2x text-gray-300"></i>
-						</div>
-					</div>
-				</div>
-			</div>
-		</a>
-
-		<a class="col-xl-3 col-md-6 mb-4" href="lista_ejercicio.php">
-			<div class="card border-left-info shadow h-100 py-2">
-				<div class="card-body">
-					<div class="row no-gutters align-items-center">
-						<div class="col mr-2">
-							<div class="text-xs font-weight-bold text-info text-uppercase mb-1">Ejercicios</div>
-							<div class="row no-gutters align-items-center">
-								<div class="col-auto">
-									<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $data['productos']; ?></div>
-								</div>
-								<div class="col">
-									<div class="progress progress-sm mr-2">
-										<div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-auto">
-							<i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-						</div>
-					</div>
-				</div>
-			</div>
-		</a>
-
-		<!-- Pending Requests Card Example -->
-		<a class="col-xl-3 col-md-6 mb-4" href="ventas.php">
-			<div class="card border-left-warning shadow h-100 py-2">
-				<div class="card-body">
-					<div class="row no-gutters align-items-center">
-						<div class="col mr-2">
-							<div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Entrenadores</div>
-							<div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $data['ventas']; ?></div>
-						</div>
-						<div class="col-auto">
-							<i class="fas fa-users fa-2x text-gray-300"></i>
-						</div>
-					</div>
-				</div>
-			</div>
-		</a>
-	</div>
-	<!-- Page Heading -->
-	<div class="d-sm-flex align-items-center justify-content-between mb-4">
 		<h1 class="h3 mb-0 text-gray-800">Configuración</h1>
 	</div>
 	<div class="row">
 		<div class="col-lg-6">
 			<div class="card">
 				<div class="card-header bg-primary text-white">
-					Información Personal
+					Información del Usuario
 				</div>
 				<div class="card-body">
 					<div class="form-group">
@@ -109,20 +26,25 @@
 					<div class="form-group">
 						<label>Usuario: <strong><?php echo $_SESSION['user']; ?></strong></label>
 					</div>
-					<ul class="list-group">
+					
+				</div>
+			</div>
+		</div>
+		<div class="col-lg-6">
+		<div class="card">
 						<li class="list-group-item active">Cambiar Contraseña</li>
 						<form action="" method=" post" name="frmChangePass" id="frmChangePass" class="p-3">
 							<div class="form-group">
 								<label>Contraseña Actual</label>
-								<input type="password" name="actual" id="actual" placeholder="Clave Actual" required class="form-control">
+								<input type="password" name="actual" id="actual" placeholder="Contraseña Actual" required class="form-control">
 							</div>
 							<div class="form-group">
 								<label>Nueva Contraseña</label>
-								<input type="password" name="nueva" id="nueva" placeholder="Nueva Clave" required class="form-control">
+								<input type="password" name="nueva" id="nueva" placeholder="Contraseña Clave" required class="form-control">
 							</div>
 							<div class="form-group">
 								<label>Confirmar Contraseña</label>
-								<input type="password" name="confirmar" id="confirmar" placeholder="Confirmar clave" required class="form-control">
+								<input type="password" name="confirmar" id="confirmar" placeholder="Confirmar Contraseña" required class="form-control">
 							</div>
 							<div class="alertChangePass" style="display:none;">
 							</div>
@@ -130,10 +52,9 @@
 								<button type="submit" class="btn btn-primary btnChangePass">Cambiar Contraseña</button>
 							</div>
 						</form>
-					</ul>
-				</div>
-			</div>
-		</div>
+					
+		</div>	
+		</div>	
 		<?php if (false) { ?>
 			<div class="col-lg-6">
 				<div class="card">
